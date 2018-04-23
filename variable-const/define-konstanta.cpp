63eff028d226c72ccb0c5b423edaabb31b1ab4a1
hariprstya <hari.prstya@gmail.com>
2018-04-23 16:49:15 +0700

    variable dan konstanta: konstanta

---
+++ variable-const/define-konstanta.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+#define panjang 30
+#define lebar 20
+
+int main(){
+  int luas = panjang * lebar;
+
+  cout << luas << endl;
+
+}
---
+++ variable-const/konstanta.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int const panjang = 30;
+  int const lebar = 20;
+  int luas = panjang*lebar;
+
+  cout << luas << endl;
+}
