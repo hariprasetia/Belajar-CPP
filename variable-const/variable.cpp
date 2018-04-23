a318bc2e98c9460c1685a65c19af3173765a2a81
hariprstya <hari.prstya@gmail.com>
2018-04-23 16:22:18 +0700

    variable dan konstanta: variable

---
+++ variable-const/variable.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+  char karakter = 'x';
+  string myString = "Luas: ";
+  int panjang = 30;
+  int lebar = 20;
+  float pi = 3.14;
+  bool truefalse;
+
+  int luas = panjang*lebar;
+
+  cout << karakter << ' ' << myString << luas << endl;
+  cout << "Pi: "<< pi << endl;
+
+}
